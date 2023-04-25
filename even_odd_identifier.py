ec366e056bca191b4e7aa85a3e599cb8f126fc7a
Kenji1504 <gamboakenleam1015@gmail.com>
2023-04-25 21:19:53 +0800

    transferring integer value to the even text file (2)

--- even_odd_identifier.py
+++ even_odd_identifier.py
@@ -4,7 +4,7 @@
 # will be headed into by identifying if it is an even or odd number.
 
 #read numbers text file, open even.txt, open odd.txt
-with open("numbers.txt", "r") as num_file, open("even.txt") as even_file, open("odd.txt") as odd_file:
+with open("numbers.txt", "r") as num_file, open("even.txt", "a") as even_file, open("odd.txt") as odd_file:
     #for each line in numbers text file
     for line in num_file:
         #convert numbers into integers
@@ -13,7 +13,7 @@ with open("numbers.txt", "r") as num_file, open("even.txt") as even_file, open("
         even_num = integer_value % 2
         #if even, append to even.txt
         if even_num == 0:
-           even_file = integer_value 
+           even_file.write(str(integer_value) + "\n")
         #if odd, append to odd.txt
         else: 
             odd_file = integer_value
