3a089696ae648abe152f2eed701ffc8223b0effb
Kenji1504 <gamboakenleam1015@gmail.com>
2023-04-25 20:08:41 +0800

    Pseudocode

---
+++ even_odd_identifier.py
@@ -0,0 +1,11 @@
+# Ken Leam G. Gamboa
+# BSCPE 1-5
+# This program will determine which text file where an integer 
+# will be headed into by identifying if it is an even or odd number.
+
+#read numbers text file, open even.txt, open odd.txt
+    #for each line in numbers text file
+    #convert numbers into integers
+    #check the integers
+    #if even, append to even.txt
+    #if odd, append to odd.txt
